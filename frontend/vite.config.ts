77b683437c00198be3cfcc7b2f6b1a0a6e58043a
Abdulvoris <abdulvoris000urolov@gmail.com>
2025-11-28 15:20:26 +0500

    deploy

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,10 +3,11 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: true,
-    port: 5173,
-    allowedHosts: true,
-  },
   base: "/admin/",
+  server: {
+    host: "0.0.0.0",
+    port: 5173,
+    allowedHosts: ["humopharmgroup.uz"], 
+    strictPort: true,
+  },
 });
